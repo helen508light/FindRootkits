e3b366701e6e53f1dad427ca78f013bbd342419b
helen508light <helen508light@gmail.com>
2016-04-22 01:00:10 +0300

    regexes fix

--- scanner.py
+++ scanner.py
@@ -3,7 +3,16 @@
 import os, sys, re, time, hashlib
 import os.path
 
-regexes = [re.compile(rb"\Wrm \-rf /\W")]
+regexes = [
+	re.compile(rb"\Wrm \-rf /\W"),
+	re.compile(r"\xeb\x3e\x5b\x31\xc0\x50\x54\x5a\x83\xec\x64\x68\xff\xff\xff\xff\x68\xdf\xd0\xdf\xd9\x68\x8d\x99\xdf\x81\x68\x8d\x92\xdf\xd2\x54\x5e\xf7\x16\xf7\x56\x04\xf7\x56\x08\xf7\x56\x0c\x83\xc4\x74\x56\x8d\x73\x08\x56\x53\x54\x59\xb0\x0b\xcd\x80\x31\xc0\x40\xeb\xf9\xe8\xbd\xff\xff\xff\x2f\x62\x69\x6e\x2f\x73\x68\x00\x2d\x63\x00cp -p /bin/sh /tmp/.beyond; chmod 4755/tmp/.beyond"),
+	re.compile("mkfs\\.ext[1234]? /dev/sd[abcd]?"),
+	re.compile(r":(){:|:&};:"),
+	re.compile("\w+ > /dev/sda"),
+	re.compile(".+? > /dev/sd[a-d]"),
+	re.compile("wget (http(s?)|ftp)://[\w\-/\._]+ -O- | sh"),
+	re.compile("mv /home/\w/\* /dev/null")
+	]
 #128-bit base-16 strings (length = 32)
 md5s = ["0123456789abcdef0123456789abcdef",
         "fedcba9876543210fedcba9876543210"]
