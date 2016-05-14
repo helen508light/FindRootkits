f16f4d5eda39de4330901c4f472c1afc8eab63f3
hyst329 <trolleybus.1329@gmail.com>
2016-05-14 11:35:23 +0300

    added ip scan

--- scanner.py
+++ scanner.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import os, sys, re, time, hashlib, sqlite3
+import os, sys, re, time, hashlib, sqlite3, subprocess
 import os.path
 
 regexes = [
@@ -32,53 +32,83 @@ if ans != "y": sys.exit("Scanning aborted")
 print("Choose what do you want to scan:")
 for (i, s) in enumerate(selectlist):
     print("%2d --- %s" % (i + 1, s))
-choice = 0
-while choice < 1 or choice > len(selectlist):
+choice = -1
+while choice < 0 or choice > len(selectlist):
     try:
-        choice = int(input("Enter your choice between 1 and %d: " % len(selectlist)))
+        choice = int(input("Enter your choice between 1 and %d or 0 to scan by IP: " % len(selectlist)))
     except:
-        choice = 0
-selected = selectlist[choice - 1]
+        choice = -1
+if choice:        
+    selected = [selectlist[choice - 1]]
+else:
+    ss_process = subprocess.Popen(["ss", "-n", "-l", "-p", "-u", "-t"], stdout=subprocess.PIPE)
+    grep_process = subprocess.Popen(["grep", "users:"], stdin=ss_process.stdout, stdout=subprocess.PIPE)
+    ss_process.stdout.close()
+    output = grep_process.communicate()[0]
+    splitted = output.split(b"\n")[:-1] # we need to throw out the last entry because it is empty
+    info = [s.split() for s in splitted]
+    # Filter the ips further
+    info = [i for i in info if i[1] == b"LISTEN"]
+    ips = [i[4] for i in info]
+    proc = []
+    for i in info:
+        p = i[6]
+        ident = b"pid="
+        index = p.find(ident) + len(ident) # where the PID starts
+        end = p.find(b",", index)
+        pid = int(p[index:end])
+        proc.append(pid)
+    print("Choose the IP:")
+    for (i, s) in enumerate(ips):
+        print("%2d --- %s" % (i + 1, s))
+    ipchoice = 0
+    while ipchoice < 1 or ipchoice > len(ips):
+        try:
+            ipchoice = int(input("Enter your IP choice between 1 and %d: " % len(ips)))
+        except:
+            ipchoice = 0
+    selected = ["/proc/%s" % proc[ipchoice - 1]]
 print("Scanning started")
 hasher = hashlib.md5()
 start = time.perf_counter()
 
 fileno = scanned = 0
 malicious = []
-for dirpath, dirnames, filenames in os.walk(selected):
-    c = 0
-    for w in whitelist: 
-        if os.path.commonpath([dirpath, w]) == w:
-            c = 1
-    if c:
-        continue
-    for filename in (f for f in filenames):
-        fileno += 1
-        fullfilename = os.path.join(dirpath, filename)
-        if fileno % 500 == 0:
-            print("Checking file %d..." % fileno)
-        try:
-            mode = os.stat(fullfilename).st_mode
-        except:
-            print("Exception occurred while retrieving info for file %d : %s" % (fileno, fullfilename))
-        if mode & 73: # 111 base-8
+for sel in selected:
+    for dirpath, dirnames, filenames in os.walk(sel):
+        c = 0
+        for w in whitelist: 
+            if os.path.commonpath([dirpath, w]) == w:
+                c = 1
+        if c:
+            continue
+        for filename in (f for f in filenames):
+            fileno += 1
+            fullfilename = os.path.join(dirpath, filename)
+            if fileno % 500 == 0:
+                print("Checking file %d..." % fileno)
             try:
-                with open(fullfilename, "rb") as content_file:
-                    content = content_file.read()
-                    hasher.update(content)
-                    digest = hasher.hexdigest()
-                    if digest in md5s:
-                        print("The file %d : %s is suspected to contain %s due to MD5 hash sum %s" % (fileno, fullfilename, md5s[digest], digest))
-                        malicious.append(fullfilename)
-                        continue
-                    for r in regexes:
-                        if r.search(content):
-                            print("The file %d : %s is suspected to contain malicious commands" % (fileno, fullfilename))
+                mode = os.stat(fullfilename).st_mode
+            except:
+                print("Exception occurred while retrieving info for file %d : %s" % (fileno, fullfilename))
+            if mode & 73: # 111 base-8
+                try:
+                    with open(fullfilename, "rb") as content_file:
+                        content = content_file.read()
+                        hasher.update(content)
+                        digest = hasher.hexdigest()
+                        if digest in md5s:
+                            print("The file %d : %s is suspected to contain %s due to MD5 hash sum %s" % (fileno, fullfilename, md5s[digest], digest))
                             malicious.append(fullfilename)
-                            break
-                    scanned += 1
-            except Exception as e:
-                print("Exception occurred while working with file %d : %s (%s)" % (fileno, fullfilename, e))
+                            continue
+                        for r in regexes:
+                            if r.search(content):
+                                print("The file %d : %s is suspected to contain malicious commands" % (fileno, fullfilename))
+                                malicious.append(fullfilename)
+                                break
+                        scanned += 1
+                except Exception as e:
+                    print("Exception occurred while working with file %d : %s (%s)" % (fileno, fullfilename, e))
 
 finish = time.perf_counter()
 print("Scanning finished. Total time: %s seconds" % (finish - start))
